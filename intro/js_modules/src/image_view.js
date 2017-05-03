b64050cebe3675bc37320b95cfe81015ae988b5e
janpauldahlke <janpauldahlke@gmail.com>
2017-05-03 07:05:04 +0200

    codesplitting

--- intro/js_modules/src/image_view.js
+++ intro/js_modules/src/image_view.js
@@ -10,15 +10,15 @@
 import '../styles/image_view.css';
 
 //image loader
-import big from '../assets/1200x1200.jpeg';
+//import big from '../assets/1200x1200.jpeg';
 import small from '../assets/350x350.png';
 
 
-const bigImage = document.createElement('img');
-const image = document.createElement('img');
+//const bigImage = document.createElement('img');
+//const image = document.createElement('img');
 //image.src = 'http://lorempixel/400/400';
-image.src = small; // works
-bigImage.src = big; //does not work // 'build/'+big //migth work but bad solution
+//image.src = small; // works
+//bigImage.src = big; //does not work // 'build/'+big //migth work but bad solution
 
 //image.src = './js_modules/assets/350x350.png';
 //unsauber, aber sonst kann das bild nicht geladen werden, sollte später in ein resolve überführt werden
@@ -27,5 +27,12 @@ bigImage.src = big; //does not work // 'build/'+big //migth work but bad solutio
   document.body.appendChild(image);
 }, 3000);*/
 //$('#entry').add(image);
-document.body.appendChild(image);
-document.body.appendChild(bigImage);
+//document.body.appendChild(image);
+//document.body.appendChild(bigImage);
+
+export default () => {
+  const image = document.createElement('img');
+  image.src = small;
+
+  document.body.appendChild(image);
+}
--- intro/js_modules/src/index.js
+++ intro/js_modules/src/index.js
@@ -3,9 +3,29 @@
 //const sum = require('./sum'); // relative path reference './'
 
 //es6 way
-import sum from './sum';
+/*import sum from './sum';
 import './image_view';
 
 console.log(sum(10,10));
 console.log(sum(1,13));
 console.log(sum(11,12));
+*/
+
+//codesplitting section
+
+
+
+const button = document.createElement('button');
+button.innerText = 'Click me';
+
+button.onclick = () => {
+  //import only on Click
+  // System.import(file) // ES6 module spec
+  System.import('./image_view'). // resolved in promise
+    then(module => {
+      //console.log(module);
+      module.default();
+    });
+};
+
+document.body.appendChild(button);
